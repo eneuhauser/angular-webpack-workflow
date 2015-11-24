66cefeca96ed20a3071f973bed2b2b9d1ec6a1ba
Eric Neuhauser <eric.neuhauser@vml.com>
2015-11-24 16:25:06 -0600

    Adds home module

--- client/app.ts
+++ client/app.ts
@@ -1,7 +1,17 @@
 import * as angular from 'angular';
+import 'angular-route';
 
+import home from './home/home.module';
 import example from './components/example/example.directive.ts';
 
 angular.module('app', [
+  'ngRoute',
+  home,
   example
-]);
+]).config(config);
+
+
+function config($routeProvider) {
+  $routeProvider.otherwise({ redirectTo: '/' });
+}
+config.$inject = [ '$routeProvider' ];
